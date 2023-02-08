db2ee3a8df5ffe0ff31436fba1dfc5c5a7683a70
mohammed <mkareem632@gmail.com>
2023-02-08 11:30:13 +0530

    she is addded

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,10 @@ package org.sample;
 
 public class Sample {
 	
+	private void she() {
+System.out.println("she is addded");
+	}
+	
 	private void mainmethod() {
 		// TODO Auto-generated method stub
 

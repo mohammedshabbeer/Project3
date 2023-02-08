dbb7b0f30f1cbbdf6a1905fd50fc74ff39b015bc
mohammed <mkareem632@gmail.com>
2023-02-08 11:28:08 +0530

    he code addded

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,10 @@ package org.sample;
 
 public class Sample {
 	
+	private void he() {
+System.out.println("he is the person");
+	}
+	
 	private void mainmethod() {
 		// TODO Auto-generated method stub
 
